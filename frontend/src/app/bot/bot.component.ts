280e3ba2fa72a72fed94d9ffdc7e9d0329768873
sumaira <sumairafarooq8@gmail.co>
2024-05-17 16:29:16 +0530

    design

--- frontend/src/app/bot/bot.component.ts
+++ frontend/src/app/bot/bot.component.ts
@@ -1,12 +1,13 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component,ViewChild,ElementRef,AfterViewChecked } from '@angular/core';
 import { SocketService } from '../service/socket.service';
 @Component({
   selector: 'app-bot',
   templateUrl: './bot.component.html',
   styleUrl: './bot.component.scss'
 })
-export class BotComponent implements OnInit {
+export class BotComponent implements AfterViewChecked {
+  @ViewChild('scrollFrame') scrollFrame!: ElementRef;
   title = 'Simple and EasyChatbot';
   messageArray: any = [];
   synth:any;
@@ -16,6 +17,18 @@ export class BotComponent implements OnInit {
     this.synth = window.speechSynthesis;
     this.voices = this.synth.getVoices();
   }
+  ngAfterViewChecked(): void {
+    this.scrollToBottom();
+  }
+
+  scrollToBottom(): void {
+    try {
+        this.scrollFrame.nativeElement.scrollTop = this.scrollFrame.nativeElement.scrollHeight;
+    } catch(err) { }
+}
+
+
+
   message: any= '';
 
   ngOnInit(){
@@ -27,6 +40,8 @@ export class BotComponent implements OnInit {
   }
   sendMessage(){
     const data = { message:this.message };
+    this.scrollToBottom();
+    this.speak(this.message);
     this.socketService.sendMessage(data);
     this.messageArray.push({name:'you', message:this.message});
     this.message = '';
@@ -41,5 +56,9 @@ export class BotComponent implements OnInit {
   u.pitch = 1; //0-2 interval
   this.synth.speak(u);
 }
+showrobotdiv(){
+
+  this.showChatDiv = !this.showChatDiv
+  }
 
 }
